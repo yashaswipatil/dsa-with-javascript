57dafeebc0e8174e147899ca4ebec6ed80de1fb6
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-07 00:49:08 +0530

    merge sorted array : second solution

--- Arrays/mergeSortedArrays.js
+++ Arrays/mergeSortedArrays.js
@@ -26,3 +26,22 @@ let res = mergeSortedArrays(nums1,m,nums2,n)
 console.log(res)
 
 //!Output => [1,2,2,3,5,6]
+
+
+//*Optimized Solution
+
+function mergeSortedArr(nums1, m ,nums2,n){
+    let p1 = m -1;
+    let p2 = n -1;
+
+    for(let i = m + m -1 ; i >= 0;i--){
+        if(p1 < 0) break;
+        if(p2 >= 0 && nums1[p1] > nums2[p2]){
+            nums1[i] = nums1[p1];
+            p1--;
+        } else {
+            nums1[i] = nums2[p1];
+            p2--;
+        }
+    }
+}
