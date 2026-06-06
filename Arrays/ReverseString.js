60df06566e3395abf8a915a7ea5ebcc66ca382b6
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-06 17:52:02 +0530

    reverse a string

---
+++ Arrays/ReverseString.js
@@ -0,0 +1,18 @@
+//*write a function to reverse a string - in-place
+
+function reverseString(arr) {
+  let len = arr.length;
+  let halfLen = Math.floor(len / 2);
+
+  for (let i = 0; i < halfLen; i++) {
+    let temp = arr[i];
+    arr[i] = arr[len - 1 - i];
+    arr[len - 1 - i] = temp;
+  }
+}
+
+let arr = ["h", "e", "l", "l", "o"];
+
+let res = reverseString(arr);
+
+console.log(res);
