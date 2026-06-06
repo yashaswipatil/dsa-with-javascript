018a1c5f4720ae30a667185aba666080f7310291
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-07 00:04:18 +0530

    merge sorted arrays : first solution

---
+++ Arrays/mergeSortedArrays.js
@@ -0,0 +1,28 @@
+//*write a function to merge sorted Arrays
+
+function mergeSortedArrays(nums1,m , nums2,n){
+    let nums1Copy = nums1.slice(0,m);
+    let p1 = 0;
+    let p2 = 0;
+
+    for(let i =0;i< m+n;i++){
+        if(p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])){
+            nums1[i] = nums1Copy[p1];
+            p1++;
+        } else {
+            nums1[i] = nums2[p2];
+            p2++;
+        }
+    }
+}
+
+let nums1 = [1,2,3,0,0,0];
+let m = 3;
+let nums2 = [2,5,6];
+let n = 3;
+
+let res = mergeSortedArrays(nums1,m,nums2,n)
+
+console.log(res)
+
+//!Output => [1,2,2,3,5,6]
