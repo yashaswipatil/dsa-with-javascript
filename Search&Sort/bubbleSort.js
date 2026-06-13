59fd2ad59974b28841b17279e6a39b105090579b
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-13 21:31:10 +0530

    bubble sort

---
+++ Search&Sort/bubbleSort.js
@@ -0,0 +1,25 @@
+//*write a function to sort an array in a non-decreasing order
+
+function bubbleSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let arr = [5, 4, 9, 1, 0];
+
+let res = bubbleSort(arr);
+
+console.log(res);
+
+//!Output => [0,1,4,5,9]
