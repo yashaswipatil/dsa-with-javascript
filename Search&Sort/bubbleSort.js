30710c3c1376add455e2e0100042b4ed3d33378b
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-13 21:50:56 +0530

    optimised solution for bubble sort

--- Search&Sort/bubbleSort.js
+++ Search&Sort/bubbleSort.js
@@ -4,12 +4,15 @@ function bubbleSort(arr) {
   let n = arr.length;
 
   for (let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        isSwapped = true;
       }
+      if(!isSwapped) break;
     }
   }
 
