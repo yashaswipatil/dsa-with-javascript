4c8054203ba75b4214b482e1b2db45e9288aa4f1
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-26 12:41:49 +0530

    swap in pairs - iterative approach(linked list)

---
+++ linkedList/swapInPairs.js
@@ -0,0 +1,33 @@
+//*Write  a function to swap in pairs of linked list 
+
+function swapPairs(head){
+    let dummy = new ListNode();
+    dummy.next = head;
+
+    let prev = dummy;
+
+    while(prev.next  && prev.next.next){
+        let first = prev.next;
+        let second = first.next;
+
+
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+
+    }
+
+    return dummy.next;
+
+
+}
+
+let head = [1, 2, 3, 4];
+
+let res = swapPairs(head);
+
+console.log(res);
+
+//!Output => [2,1,4,3];
