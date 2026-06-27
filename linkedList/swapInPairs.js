2be7cd434eeb7a720de86060154afee8229ec5a9
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-27 11:58:50 +0530

    swap in pairs - recursive approach

--- linkedList/swapInPairs.js
+++ linkedList/swapInPairs.js
@@ -26,10 +26,26 @@ function swapPairs(head){
 
 }
 
+
+function swapPairs2(head){
+    if(!head || !head) return head;
+
+    let left = head;
+    let right = head.next;
+
+    left.next = swapPairs(right.next);
+    right.next = left;
+
+
+    return right;
+}
+
 let head = [1, 2, 3, 4];
 
 let res = swapPairs(head);
+let res2 = swapPairs2(head);
 
 console.log(res);
+console.log(res2)
 
 //!Output => [2,1,4,3];
