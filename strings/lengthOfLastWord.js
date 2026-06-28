b85a15c5303e8e357b2bf6f105f14b2fe96b79ed
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-28 17:21:07 +0530

    string:- one loop approach for length of last word

--- strings/lengthOfLastWord.js
+++ strings/lengthOfLastWord.js
@@ -24,8 +24,25 @@ function lengthOfTheWord(s){
     return length;
 }
 
+function lengthOfTheWord2(s){
+    let length = 0;
+    let n = s.length - 1;
+
+    while(n>=0){
+        if(s[n] !== " "){
+            length++;
+        } else if(length > 0){
+            break;
+        }
+        n--;
+    }
+
+    return length;
+}
+
 let s = "Hello World";
 let res = lengthOfTheWord(s);
+let res2 = lengthOfTheWord2(s);
 
 console.log(res);
 
