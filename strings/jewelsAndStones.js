8554c316584035904d01e779b7fa270a80f6eb50
Yesashwini Patil <yashaswipatil75@gmail.com>
2026-06-30 11:50:33 +0530

    strings : jewels and stones - approach 2

--- strings/jewelsAndStones.js
+++ strings/jewelsAndStones.js
@@ -14,11 +14,29 @@ function jewelsAndStones(jewels,stones){
     return count;
 }
 
+function jewelsAndStones2(jewels,stones){
+    let count = 0;
+    let jSet = new Set();
+    for(let i = 0; i < jewels.length;i++){
+        jSet.add(jewels[i])
+    }
+
+    for(let i = 0; i < stones.length;i++){
+        if(jSet.has(stones[i])){
+            count++;
+        }
+    }
+
+    return count;
+}
+
 let jewels = "aA";
 let stones = "aAAbbbb";
 
 let res = jewelsAndStones(jewels,stones);
+let res2 = jewelsAndStones2(jewels, stones);
 
 console.log(res);
+console.log(res2);
 
 //!Output => 3
